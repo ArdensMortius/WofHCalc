5bee0ccdaa99327ce463b522ca76e92edf3224b4
Ardens Mortius <stasanurin37@gmail.com>
2023-05-13 12:05:34 +0300

    new Window Finance

---
+++ WofHCalc p3 (B)/Views/Finance.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WofHCalc.Views
+{
+    /// <summary>
+    /// Interaction logic for Finance.xaml
+    /// </summary>
+    public partial class Finance : Window
+    {
+        public Finance()
+        {
+            InitializeComponent();
+        }
+    }
+}
