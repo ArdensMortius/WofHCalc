501d1d2916817f43ebab90190b467eadcb773b3f
Ardens Mortius <stasanurin37@gmail.com>
2023-08-25 14:40:30 +0300

    add unitnames enum

--- WofHCalc p3 (B)/Supports/enums.cs
+++ WofHCalc p3 (B)/Supports/enums.cs
@@ -166,6 +166,230 @@ namespace WofHCalc.Supports
         [Description("Исслед. аква")]
         ResearchWaterArea = 9 //аква
     }
+    public enum UnitsNames
+    {
+        [Description("Воин")]
+        Warrior = 0,
+        [Description("Пращник")]
+        Slinger = 1,
+        [Description("Копейщик")]
+        Lance_knight = 2,
+        [Description("Топорщик")]
+        Axeman = 3,
+        [Description("Пикинер")]
+        Pikeman = 4,
+        [Description("Бомбарда")]
+        Bombard = 5,
+        [Description("Бомбардирский_корабль")]
+        Bombardier_ship = 6,
+        [Description("Пароход")]
+        Steamboat = 7,
+        [Description("Метатель_дротиков")]
+        Javelin_thrower = 8,
+        [Description("Трирема")]
+        Trireme = 9,
+        [Description("Ладья")]
+        Bark = 10,
+        [Description("Боевой_слон")]
+        Battle_elephant = 11,
+        [Description("Дубинщик")]
+        Maceman = 12,
+        [Description("Всадник")]
+        Horseman = 13,
+        [Description("Бронеавтомобиль")]
+        Armored_car = 14,
+        [Description("Драгун")]
+        Dragoon = 15,
+        [Description("Колесница")]
+        Chariot = 16,
+        [Description("Автоматчик")]
+        Submachine_gunner = 17,
+        [Description("Пулеметчик")]
+        Machine_gunner = 18,
+        [Description("Лёгкий_танк")]
+        Light_tank = 19,
+        [Description("Метатель_бола")]
+        Bola_thrower = 20,
+        [Description("Дивизионная_пушка")]
+        Squadron_cannon = 21,
+        [Description("Солдат")]
+        Soldier = 22,
+        [Description("Длинный_лук")]
+        Longbow = 23,
+        [Description("Кирасир")]
+        Cuirassier = 24,
+        [Description("Мушкетер")]
+        Musketeer = 25,
+        [Description("Егерь")]
+        Ranger = 26,
+        [Description("Кавалерист")]
+        Cavalryman = 27,
+        [Description("Алебардщик")]
+        Halberdier = 28,
+        [Description("Арбалетчик")]
+        Crossbowman = 29,
+        [Description("Гренадер")]
+        Grenadier = 30,
+        [Description("Современный_танк")]
+        Modern_tank = 31,
+        [Description("Рыцарь")]
+        Knight = 32,
+        [Description("Самурай")]
+        Samurai = 33,
+        [Description("Галера")]
+        Galley = 34,
+        [Description("Разведчик")]
+        Scout = 35,
+        [Description("Лучник")]
+        Archer = 36,
+        [Description("Мечник")]
+        Swordsman = 37,
+        [Description("Пушка")]
+        Cannon = 38,
+        [Description("Мортира")]
+        Mortar = 39,
+        [Description("Гвардеец")]
+        Guardsman = 40,
+        [Description("Катапульта")]
+        Catapult = 41,
+        [Description("Рутта")]
+        Springald = 42,
+        [Description("Поселенец")]
+        Settler = 43,
+        [Description("Моргенштерн")]
+        Morgenstern = 44,
+        [Description("Онагр")]
+        Onager = 45,
+        [Description("Требушет")]
+        Trebuchet = 46,
+        [Description("Гранатометчик")]
+        Grenade_launcher = 47,
+        [Description("Лодка")]
+        Boat = 48,
+        [Description("Каноэ")]
+        Canoe = 49,
+        [Description("Пехотинец")]
+        Infantry = 50,
+        [Description("Конный_лучник")]
+        Horse_archer = 51,
+        [Description("Конный_арбалетчик")]
+        Horse_crossbowman = 52,
+        [Description("Каравелла")]
+        Caravel = 53,
+        [Description("Фрегат")]
+        Frigate = 54,
+        [Description("Галеон")]
+        Galleon = 55,
+        [Description("Рабочий")]
+        Worker = 56,
+        [Description("Ополченец")]
+        Militiaman = 57,
+        [Description("Реактивная_артиллерия")]
+        Rocket_artillery = 58,
+        [Description("Боевая_машина пехоты")]
+        Infantry_fighting_vehicle = 59,
+        [Description("КК_Двигатель")]
+        SS_Engine = 60,
+        [Description("КК_Рубка")]
+        SS_Cockpit = 61,
+        [Description("КК_Обшивка")]
+        SS_Hull = 62,
+        [Description("КК_Топливный бак")]
+        SS_Fuel_tank = 63,
+        [Description("КК_Солнечная батарея")]
+        SS_Solarcell_array = 64,
+        [Description("КК_Модуль обеспечения")]
+        SS_Support_module = 65,
+        [Description("Броненосец")]
+        Ironclad = 66,
+        [Description("Эсминец")]
+        Destroyer = 67,
+        [Description("Транспортный_корабль")]
+        Transport_ship = 68,
+        [Description("Дредноут")]
+        Dreadnought = 69,
+        [Description("Подводная_лодка")]
+        Submarine = 70,
+        [Description("Линкор")]
+        Battleship = 71,
+        [Description("Атомная_подводная лодка")]
+        Nuclear_submarine = 72,
+        [Description("Крейсер")]
+        Cruiser = 73,
+        [Description("Дирижабль")]
+        Dirigible = 74,
+        [Description("Робопёс")]
+        Robodog = 75,
+        [Description("Снайпер")]
+        Sniper = 76,
+        [Description("Джонка")]
+        Junk = 77,
+        [Description("Легионер")]
+        Legionary = 78,
+        [Description("Воинягуар")]
+        Jaguarwarrior = 79,
+        [Description("Чокону")]
+        Chukonu = 80,
+        [Description("Мотоциклист")]
+        Motorcyclist = 81,
+        [Description("Легкий_истребитель")]
+        Lightweight_fighter = 82,
+        [Description("Легкий_бомбардировщик")]
+        Light_bomber = 83,
+        [Description("Пикировщик")]
+        Dive_bomber = 84,
+        [Description("Аэростат")]
+        Aerostat = 85,
+        [Description("Зенитное_орудие")]
+        Air_defense_gun = 86,
+        [Description("Тачанка_ПВО")]
+        Mobile_SAM = 87,
+        [Description("Танкягуар")]
+        Jaguar_tank = 88,
+        [Description("Истребитель")]
+        Fighter_aircraft = 89,
+        [Description("Бомбардировщик")]
+        Bomber_aircraft = 90,
+        [Description("Штурмовик")]
+        Attack_aircraft = 91,
+        [Description("Двухместная_колесница")]
+        TwoSeat_Chariot = 92,
+        [Description("Скользкий_Крыс")]
+        Slippery_Rat = 93,
+        [Description("Реактивный_истребитель")]
+        Jet_fighter = 94,
+        [Description("Реактивный_бомбардировщик")]
+        Jet_bomber = 95,
+        [Description("Импи")]
+        Impi = 96,
+        [Description("Мамлюк")]
+        Mamluk = 97,
+        [Description("Папирусная_лодка")]
+        Papyrus_Boat = 98,
+        [Description("Латник")]
+        Landsknecht = 99,
+        [Description("Минометчик")]
+        Trenchmortar = 100,
+        [Description("Тяжелый_танк")]
+        Heavy_tank = 101,
+        [Description("Бронетранспортер")]
+        APC = 102,
+        [Description("Реактивный_миномет")]
+        Rocket_launcher = 103,
+        [Description("Огнеметчик")]
+        Flamethrower = 104,
+        [Description("ЗРПК")]
+        SPAAG = 105,
+        [Description("Резервный_юнит")]
+        Reserved = 110,
+        [Description("Дед")]
+        Santa = 111,
+        [Description("Внучка")]
+        Snowflake = 112,
+        [Description("Лось")]
+        Deer = 113
+    }
+
     public enum DepositName //++
     {
         [Description("Нет")]
@@ -449,7 +673,7 @@ namespace WofHCalc.Supports
         fountain = 34, //
         [Description("водопровод")]
         aqueduct = 71, //
-        [Description("водазабор")]
+        [Description("водозабор")]
         water_intake = 28, //
         [Description("больница")]
         hospital = 37, //
@@ -481,7 +705,7 @@ namespace WofHCalc.Supports
         Earthen_dam = 76,
         [Description("Геоглиф")]
         Geoglyph = 56,
-        [Description("Мать-Земля")]
+        [Description("МатьЗемля")]
         Earth_Mother = 43,
         [Description("Висячие сады")]
         The_Hanging_Gardens = 92,
@@ -497,7 +721,7 @@ namespace WofHCalc.Supports
         The_Terracotta_Army = 9,
         [Description("Колосс")]
         The_Colossus = 75,
-        [Description("Мачу-Пикчу")]
+        [Description("МачуПикчу")]
         Machu_Picchu = 100,
         [Description("Гелиоконцентратор")]
         Helioconcentrator = 91,
@@ -505,11 +729,11 @@ namespace WofHCalc.Supports
         The_Order_of_Knights_Templar = 98,
         [Description("Академия СунЦзы")]
         Sun_Tzus_War_Academy = 99,
-        [Description("Царь-пушка")]
+        [Description("Царьпушка")]
         The_Tsar_Cannon = 95,
         [Description("Бранденбургские ворота")]
         Brandenburg_Gate = 108,
-        [Description("Лас-Вегас")]
+        [Description("ЛасВегас")]
         LasVegas = 107,
         [Description("Грузовой аэропорт")]
         Freight_airport = 30,
